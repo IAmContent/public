789896d4c473bb10237bd79b22495d79784ee5b5
IAmContent <greg@jarchitect.co.uk>
2015-04-25 20:34:21 +0100

    Enhance UsbTestDriver to accept arguments instead of hard-coded ids.

--- public-java/iamcontent-io/src/test/java/com/iamcontent/io/usb/UsbTestDriver.java
+++ public-java/iamcontent-io/src/test/java/com/iamcontent/io/usb/UsbTestDriver.java
@@ -17,11 +17,49 @@
  */
 package com.iamcontent.io.usb;
 
+/**
+ * Searches for a USB device with a given vendor id and product id.
+ * @author Greg Elderfield
+ */
 public class UsbTestDriver {
-	public static final short VENDOR_ID = 0x1267;
-	public static final short PRODUCT_ID = 0x0;
 
 	public static void main(String[] args) {
-		System.out.println(Usb.device(VENDOR_ID, PRODUCT_ID));
+		try {
+			System.out.println(seekDevice(args));
+		} catch (ArrayIndexOutOfBoundsException e) {
+			usage("Too few parameters.");
+		} catch (NumberFormatException e) {
+			usage("Illegal number.");
+		}
+	}
+
+	private static EasyUsbDevice seekDevice(String[] args) {
+		return seekDevice(args(args));
+	}
+
+	private static EasyUsbDevice seekDevice(final Args a) {
+		return Usb.device(a.vendorId, a.productId);
+	}
+	
+	private static Args args(String[] args) {
+		return new Args(args);
+	}
+	
+	/**
+	 * Parses the arguments.
+	 */
+	private static class Args {
+		final Short vendorId;
+		final Short productId;
+		public Args(String[] args) {
+			vendorId = Short.parseShort(args[0]);
+			productId = Short.parseShort(args[1]);
+		}
+	}
+	
+	private static void usage(final String reason) {
+		System.err.println(reason);
+		System.err.println("Usage: java -cp<classpath>" + UsbTestDriver.class.getName() + " <vendorId> <productId>" );
+		System.err.println("Please try again.");
 	}
 }
